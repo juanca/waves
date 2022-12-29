886980c31503524c5a46a778bb4dffd3867ad0c9
Juan Carlos Medina <jmedina12@cmc.edu>
2022-12-29 14:26:40 -0800

    rails generate model Comment message:string post:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_235559) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_29_222625) do
+  create_table "comments", force: :cascade do |t|
+    t.string "message"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
