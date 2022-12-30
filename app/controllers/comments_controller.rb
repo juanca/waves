7989ca74d64b32b829da4d0720e8ff3a7b9f4722
Juan Carlos Medina <jmedina12@cmc.edu>
2022-12-30 11:43:22 -0800

    feat(post): a comment can be deleted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,16 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to comment.post, notice: "Comment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy], shallow: true
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
