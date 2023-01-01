a1926844bc3fe86863d940e7b18cd752f7c3abdd
Juan Carlos Medina <jmedina12@cmc.edu>
2023-01-01 14:15:53 -0800

    feat(post): a post can have a reference to a resource
    This will be used to show a fancy preview per post. Like a link to a blog, an image, or a video.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :message, presence: true, length: { maximum: 255 }
+  validates :reference, format: URI::DEFAULT_PARSER.make_regexp(%w[https])
 
   has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_29_222625) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_01_220938) do
   create_table "comments", force: :cascade do |t|
     t.string "message"
     t.integer "post_id", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_29_222625) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reference"
   end
 
   add_foreign_key "comments", "posts"
