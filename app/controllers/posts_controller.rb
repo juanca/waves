d4f0c77ab2f857a160672b495bdad1dc9d046c9a
Juan Carlos Medina <jmedina12@cmc.edu>
2023-01-01 18:34:36 -0800

    feat(post): post form can submit a reference

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:message)
+      params.require(:post).permit(:message, :reference)
     end
 end
