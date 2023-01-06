4c7bbb598c5798ca7cd1254d062f271f5797af05
Juan Carlos Medina <jmedina12@cmc.edu>
2023-01-05 17:07:42 -0800

    fix(post): allow blank references, use specific error messaging

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   validates :message, presence: true, length: { maximum: 255 }
-  validates :reference, format: URI::DEFAULT_PARSER.make_regexp(%w[https])
+  validates :reference, format: {
+    allow_blank: true,
+    message: "only allows URLs",
+    with: URI::DEFAULT_PARSER.make_regexp(%w[https]),
+  }
 
   has_many :comments, dependent: :destroy
 end
